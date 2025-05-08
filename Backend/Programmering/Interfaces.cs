7132fcec9c49bbc3139d93b2ac049b035e18181e
Nonolar <08noelar@skola.boras.se>
2025-05-08 16:40:38 +0200

    Removed placeholder and defined all interfaces for further use

---
+++ Backend/Programmering/Interfaces.cs
@@ -0,0 +1,22 @@
+using System.Net;
+
+interface HTTPDecoder{
+    //public event EventHandler GotHTTP;
+    public string GetLatestRequest();
+    public bool SendResponse(byte[] bytes);
+    public byte[] JsonToByte(string Json);
+}
+
+interface Tokens{
+    public enum Tokens;
+    public Tokens[] Parse(string JSON);
+}
+interface Parser{
+    public enum Stmt;
+    public  Stmt[] Parse(Enum[] Tokens);
+}
+
+
+interface Generator{
+    public string Generate(Enum[] Stmnts);
+}
